89c740cf94d2ba655f51d7de5e1f235565b56846
DJ-Sandoval <sandovalsantanajosearmando@gmail.com>
2026-07-28 00:28:33 -0600

    primer commit configuramos la config principal del proyecto, base de datos, docker, kubernetes, terraform, .env

---
+++ src/main/java/com/dev/apitaconube/ApitaconubeApplication.java
@@ -0,0 +1,13 @@
+package com.dev.apitaconube;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApitaconubeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApitaconubeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dev/apitaconube/ApitaconubeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.apitaconube;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApitaconubeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
